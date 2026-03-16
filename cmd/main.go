285480126ff373c805a0fca8009aa2fe1b0fa05d
lazarevFedor <lazarevfedor3@gmail.com>
2026-03-16 13:17:58 +0300

    feat: add request context middleware and initialize gin router

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joaziz/go-gin-graceful-shutdown/graceful"
+)
+
+func RequestContextMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func main() {
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(RequestContextMiddleware())
+	graceful.New(router, 30*time.Second).ListenAndServe(":8080")
 }
