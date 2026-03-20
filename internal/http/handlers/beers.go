461abd8011029e065513f10c758ecd79b818e104
lazarevFedor <lazarevfedor3@gmail.com>
2026-03-20 23:20:31 +0300

    feat(beers): implement CRUD operations for beer categories and beers

--- internal/http/handlers/beers.go
+++ internal/http/handlers/beers.go
@@ -1,8 +1,26 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"Brewery/pkg/logger"
+	"net/http"
 
-type beerClassifier struct {
+	"github.com/gin-gonic/gin"
+)
+
+type beerClassifier interface {
+	CreateCategory(c *gin.Context)
+	GetCategoryById(c *gin.Context)
+	UpdateCategory(c *gin.Context)
+	DeleteCategory(c *gin.Context)
+	GetAllCategories(c *gin.Context)
+
+	GetParentCategory(c *gin.Context)
+	GetChildCategory(c *gin.Context)
+
+	CreateBeer(c *gin.Context)
+	GetBeerById(c *gin.Context)
+	UpdateBeer(c *gin.Context)
+	DeleteBeer(c *gin.Context)
 }
 
 type BeersHandler struct {
@@ -15,8 +33,123 @@ func NewBeersHandler(bc beerClassifier) *BeersHandler {
 	}
 }
 
-func (h BeersHandler) Create(_ *gin.Context) {}
-func (h BeersHandler) Get(_ *gin.Context)    {}
-func (h BeersHandler) Update(_ *gin.Context) {}
-func (h BeersHandler) Delete(_ *gin.Context) {}
-func (h BeersHandler) List(_ *gin.Context)   {}
+func (h *BeersHandler) CreateCategory(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get logger from context"})
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusCreated)
+}
+
+func (h *BeersHandler) GetCategoryById(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get logger from context"})
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) UpdateCategory(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get logger from context"})
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) DeleteCategory(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get logger from context"})
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) GetAllCategories(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) GetParentCategory(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) GetChildCategory(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) CreateBeer(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusCreated)
+}
+
+func (h *BeersHandler) GetBeerById(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) UpdateBeer(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
+
+func (h *BeersHandler) DeleteBeer(c *gin.Context) {
+	log, ok := logger.GetLoggerFromCtx(c.Request.Context())
+	if !ok {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	log.Info(c.Request.Context(), "")
+	c.Status(http.StatusOK)
+}
