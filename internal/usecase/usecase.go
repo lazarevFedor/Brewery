a75e273e78b12ef57435519edf407d8fd09a045b
lazarevFedor <lazarevfedor3@gmail.com>
2026-03-21 12:10:40 +0300

    feat(usecase): update GetAllBeers method to return []entities.Beer instead of []entities.ProductCategory

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -20,15 +20,15 @@ type BeerService interface {
 	GetBeerById(ctx context.Context, id int) (*entities.Beer, error)
 	UpdateBeer(ctx context.Context, id int) error
 	DeleteBeer(ctx context.Context, id int) error
-	GetAllBeers(ctx context.Context) ([]entities.ProductCategory, error)
+	GetAllBeers(ctx context.Context) ([]entities.Beer, error)
 }
 
 type beerService struct {
-	beerRepo     *repository.BeerRepository
+	beerRepo     repository.BeerRepository
 	categoryRepo interface{} // TODO: заменить на интерфейс CategoryRepository когда он появится
 }
 
-func NewBeerService(beerRepo *repository.BeerRepository, categoryRepo interface{}) BeerService {
+func NewBeerService(beerRepo repository.BeerRepository, categoryRepo interface{}) BeerService {
 	return &beerService{
 		beerRepo:     beerRepo,
 		categoryRepo: categoryRepo,
@@ -36,49 +36,71 @@ func NewBeerService(beerRepo *repository.BeerRepository, categoryRepo interface{
 }
 
 func (s *beerService) CreateCategory(ctx context.Context, cat *entities.ProductCategory) (int, error) {
+	_ = ctx
+	_ = cat
 	return 0, nil
 }
 
 func (s *beerService) GetCategoryById(ctx context.Context, id int) (*entities.ProductCategory, error) {
+	_ = ctx
+	_ = id
 	return nil, nil
 }
 
 func (s *beerService) UpdateCategory(ctx context.Context, id int) error {
+	_ = ctx
+	_ = id
 	return nil
 }
 
 func (s *beerService) DeleteCategory(ctx context.Context, id int) error {
+	_ = ctx
+	_ = id
 	return nil
 }
 
 func (s *beerService) GetAllCategories(ctx context.Context) ([]entities.ProductCategory, error) {
+	_ = ctx
 	return nil, nil
 }
 
 func (s *beerService) GetParentCategory(ctx context.Context, id int) (*entities.ProductCategory, error) {
+	_ = ctx
+	_ = id
 	return nil, nil
 }
 
 func (s *beerService) GetChildCategory(ctx context.Context, id int) (*entities.ProductCategory, error) {
+	_ = ctx
+	_ = id
 	return nil, nil
 }
 
 func (s *beerService) CreateBeer(ctx context.Context, beer *entities.Beer) (int, error) {
+	_ = ctx
+	_ = beer
 	return 0, nil
 }
 
 func (s *beerService) GetBeerById(ctx context.Context, id int) (*entities.Beer, error) {
+	_ = ctx
+	_ = id
 	return nil, nil
 }
 
 func (s *beerService) UpdateBeer(ctx context.Context, id int) error {
+	_ = ctx
+	_ = id
 	return nil
 }
 
 func (s *beerService) DeleteBeer(ctx context.Context, id int) error {
+	_ = ctx
+	_ = id
 	return nil
 }
 
-func (s *beerService) GetAllBeers(ctx context.Context) ([]entities.ProductCategory, error) {
+func (s *beerService) GetAllBeers(ctx context.Context) ([]entities.Beer, error) {
+	_ = ctx
 	return nil, nil
 }
