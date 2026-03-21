6c9cdcce634bf2648bb6cc96a37cc62f1fee6e37
lazarevFedor <lazarevfedor3@gmail.com>
2026-03-21 12:20:26 +0300

    fix(review): correct formatting of struct fields in Review type

--- internal/entities/review.go
+++ internal/entities/review.go
@@ -2,8 +2,8 @@ package entities
 
 // Review представляет полное описание отзыва о пиве
 type Review struct {
-	ID     int `json:"id" info:"ID записи в бд"`
-	Body   string `json:"body" info:"Комментарий отзыва"`
-	BeerID uint `json:"beer_id" info:"ID напитка, к которому прикреплен отзыв"`
+	ID     int     `json:"id" info:"ID записи в бд"`
+	Body   string  `json:"body" info:"Комментарий отзыва"`
+	BeerID uint    `json:"beer_id" info:"ID напитка, к которому прикреплен отзыв"`
 	Rating float32 `json:"rating" info:"Оценка напитка"`
 }
