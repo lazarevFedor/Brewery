7292039ca5e2791fc95fe8712aeb9cf160cbc9e4
Kirill Belyaev <kirill10121415@gmail.com>
2026-03-25 01:33:23 +0300

    feat(db): add categories repository

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Brewery
 go 1.26.1
 
 require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/gin-contrib/graceful v1.2.0
 	github.com/gin-gonic/gin v1.12.0
 	github.com/google/uuid v1.6.0
@@ -61,6 +62,8 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.18.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.10 // indirect
--- internal/repository/category/category_repository.go
+++ internal/repository/category/category_repository.go
@@ -3,8 +3,17 @@ package category
 import (
 	"Brewery/internal/entities"
 	"context"
+	"fmt"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	tableCategories = "product_categories"
+	IDCol           = "id"
+	nameCol         = "name"
+	parentIDCol     = "parent_id"
 )
 
 type CategoryRepository interface {
@@ -20,10 +29,114 @@ func NewCategoryPostgres(pool *pgxpool.Pool) *CategoryPostgres {
 	return &CategoryPostgres{pool: pool}
 }
 
+func (r *CategoryPostgres) GetCategories(ctx context.Context) ([]entities.ProductCategory, error) {
+	builder := sq.Select(IDCol, nameCol, parentIDCol).From(tableCategories)
+
+	psql := builder.PlaceholderFormat(sq.Dollar)
+	query, args, err := psql.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("ToSql: %w", err)
+	}
+
+	rows, err := r.pool.Query(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("Query: %w", err)
+	}
+
+	categories := make([]entities.ProductCategory, 0)
+	for rows.Next() {
+		ctg := entities.ProductCategory{}
+
+		err = rows.Scan(&ctg.ID, &ctg.Name, &ctg.ParentID)
+		if err != nil {
+			return nil, fmt.Errorf("Scan: %w", err)
+		}
+		categories = append(categories, ctg)
+	}
+	return categories, nil
+}
+
 func (r *CategoryPostgres) InsertCategory(ctx context.Context, category entities.ProductCategory) error {
+	data := map[string]interface{}{
+		nameCol:     category.Name,
+		parentIDCol: category.ParentID,
+	}
+	builder := sq.Insert(tableCategories).SetMap(data)
+	psql := builder.PlaceholderFormat(sq.Dollar)
+	query, args, err := psql.ToSql()
+	if err != nil {
+		return fmt.Errorf("ToSql: %w", err)
+	}
+
+	_, err = r.pool.Exec(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("Exec: %w", err)
+	}
 	return nil
 }
 
-func (r *CategoryPostgres) DeleteCategoryByName(ctx context.Context, name string) error {
+func (r *CategoryPostgres) GetCategoryById(ctx context.Context, id int) (*entities.ProductCategory, error) {
+	data := map[string]interface{}{
+		IDCol: id,
+	}
+	builder := sq.Select(IDCol, nameCol, parentIDCol).Where(data)
+	psql := builder.PlaceholderFormat(sq.Dollar)
+	query, args, err := psql.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("ToSql: %w", err)
+	}
+
+	var ctg entities.ProductCategory
+	err = r.pool.QueryRow(ctx, query, args...).Scan(&ctg.ID, &ctg.Name, &ctg.ParentID)
+	if err != nil {
+		return nil, fmt.Errorf("QueryRow: %w", err)
+	}
+
+	return &ctg, nil
+}
+
+func (r *CategoryPostgres) UpdateCategory(ctx context.Context, id int, updates map[string]interface{}) error {
+	builder := sq.Update(tableCategories).
+		SetMap(updates).
+		Where(sq.Eq{IDCol: id})
+	psql := builder.PlaceholderFormat(sq.Dollar)
+	query, args, err := psql.ToSql()
+	if err != nil {
+		return fmt.Errorf("ToSql: %w", err)
+	}
+
+	result, err := r.pool.Exec(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("QueryRow: %w", err)
+	}
+
+	if result.RowsAffected() == 0{
+		return fmt.Errorf("failed to update category: no such category")
+	}
+
+	return nil
+}
+
+func (r *CategoryPostgres) DeleteCategoryByID(ctx context.Context, id int) error {
+	conditions := map[string]interface{}{
+		IDCol: id,
+	}
+	builder := sq.Delete(tableCategories).Where(conditions)
+	psql := builder.PlaceholderFormat(sq.Dollar)
+
+	query, args, err := psql.ToSql()
+	if err != nil {
+		return fmt.Errorf("ToSql: %w", err)
+	}
+
+	result, err := r.pool.Exec(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("Exec: %w", err)
+	}
+
+	if result.RowsAffected() == 0 {
+		return fmt.Errorf("failed to delete category: no such category")
+	}
+
 	return nil
 }
