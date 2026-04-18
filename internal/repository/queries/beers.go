60d77b97b76f9fc8653f8e0bfb43e6078aaeb44e
fedorlazarev <lazarevfedor3@gmail.com>
2026-04-18 14:19:56 +0300

    feat(db): add comments to query functions for improved clarity

--- internal/repository/queries/beers.go
+++ internal/repository/queries/beers.go
@@ -1,3 +1,4 @@
+// Package queries содержит функции для сборки запросов к базе данных
 package queries
 
 import (
@@ -15,8 +16,10 @@ const (
 	beerFeaturesTable = "beer_features"
 )
 
+// psql - это экземпляр StatementBuilder, настроенный на использование формата плейсхолдеров для PostgreSQL.
 var psql = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
 
+// FullBeerSelect возвращает базовый запрос для получения полной информации о пиве, включая его характеристики, город и страну производства, категорию и особенности.
 func FullBeerSelect() sq.SelectBuilder {
 	return psql.Select(
 		"b.id",
@@ -45,16 +48,19 @@ func FullBeerSelect() sq.SelectBuilder {
 		)
 }
 
+// SelectBeerByID возвращает запрос для получения информации о пиве по его ID.
 func SelectBeerByID(id uint) sq.SelectBuilder {
 	return FullBeerSelect().Where(sq.Eq{"b.id": id})
 }
 
+// SelectBeerByCategoryID возвращает запрос для получения списка пива, принадлежащего к определенной категории, с сортировкой по убыванию ID.
 func SelectBeerByCategoryID(categoryID uint) sq.SelectBuilder {
 	return FullBeerSelect().
 		Where(sq.Eq{"category_id": categoryID}).
 		OrderBy("id DESC")
 }
 
+// InsertReview возвращает запрос для вставки нового отзыва в таблицу reviews и возвращает ID вставленного отзыва.
 func InsertReview(review entities.Review) sq.InsertBuilder {
 	data := map[string]any{
 		"body":    review.Body,
@@ -68,12 +74,14 @@ func InsertReview(review entities.Review) sq.InsertBuilder {
 		Suffix("RETURNING id")
 }
 
+// DeleteBeer возвращает запрос для удаления пива из таблицы beers по его ID.
 func DeleteBeer(id uint) sq.DeleteBuilder {
 	return psql.
 		Delete(beersTable).
 		Where(sq.Eq{"id": id})
 }
 
+// UpdateBeer возвращает запрос для обновления информации о пиве в таблице beers по его ID с использованием данных из переданной карты обновлений.
 func UpdateBeer(id uint, updates map[string]any) sq.UpdateBuilder {
 	return psql.
 		Update(beersTable).
@@ -82,6 +90,7 @@ func UpdateBeer(id uint, updates map[string]any) sq.UpdateBuilder {
 		Suffix("RETURNING id")
 }
 
+// SelectOrInsertCountry возвращает запрос для вставки новой страны в таблицу countries, если страна с таким именем еще не существует, или возвращает ID существующей страны, если она уже есть. Запрос использует конструкцию ON CONFLICT для обработки конфликтов по имени страны.
 func SelectOrInsertCountry(name string) sq.InsertBuilder {
 	data := map[string]any{
 		"name": name,
@@ -95,6 +104,7 @@ func SelectOrInsertCountry(name string) sq.InsertBuilder {
 			"RETURNING id")
 }
 
+// SelectOrInsertCity возвращает запрос для вставки нового города в таблицу cities, если город с таким именем и идентификатором страны еще не существует, или возвращает ID существующего города, если он уже есть. Запрос использует конструкцию ON CONFLICT для обработки конфликтов по имени города и идентификатору страны.
 func SelectOrInsertCity(cityName string, countryID uint) sq.InsertBuilder {
 	data := map[string]any{
 		"name":       cityName,
@@ -109,6 +119,7 @@ func SelectOrInsertCity(cityName string, countryID uint) sq.InsertBuilder {
 			"RETURNING id")
 }
 
+// SelectOrInsertFeature возвращает запрос для вставки новой особенности в таблицу features, если особенность с таким именем еще не существует, или возвращает ID существующей особенности, если она уже есть. Запрос использует конструкцию ON CONFLICT для обработки конфликтов по имени особенности.
 func SelectOrInsertFeature(featName string) sq.InsertBuilder {
 	data := map[string]any{
 		"name": featName,
@@ -122,6 +133,7 @@ func SelectOrInsertFeature(featName string) sq.InsertBuilder {
 			"RETURNING id")
 }
 
+// SelectOrInsertBeerFeature возвращает запрос для вставки новой связи между пивом и особенностью в таблицу beer_features, если такая связь еще не существует, или ничего не делает, если связь уже есть. Запрос использует конструкцию ON CONFLICT для обработки конфликтов по идентификаторам пива и особенности.
 func SelectOrInsertBeerFeature(featID, beerID uint) sq.InsertBuilder {
 	data := map[string]any{
 		"beer_id":    beerID,
@@ -134,6 +146,7 @@ func SelectOrInsertBeerFeature(featID, beerID uint) sq.InsertBuilder {
 		Suffix("ON CONFLICT DO NOTHING")
 }
 
+// InsertBeer возвращает запрос для вставки нового пива в таблицу beers с использованием данных из переданной структуры beer и идентификаторов города и категории. Запрос возвращает ID вставленного пива.
 func InsertBeer(beer entities.Beer, cityID, categoryID uint) sq.InsertBuilder {
 	data := map[string]any{
 		"name":        beer.Name,
