adabf7c04b9f4bde1e9ae7147e0fe5fdbd5fdae6
fedorlazarev <lazarevfedor3@gmail.com>
2026-04-19 14:30:03 +0300

    feat(db): make enum table fields NOT NULL for improved data integrity

--- migrator/sql/00003_init_enum_tables.sql
+++ migrator/sql/00003_init_enum_tables.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE IF NOT EXISTS enum_classes (
     id INT PRIMARY KEY,
-    enum_type VARCHAR(100),
-    entity_name VARCHAR(50),
-    field_name VARCHAR(50),
+    enum_type VARCHAR(100) NOT NULL,
+    entity_name VARCHAR(50) NOT NULL,
+    field_name VARCHAR(50) NOT NULL,
     UNIQUE (entity_name, field_name)
 
 );
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS enum_values (
     value_raw VARCHAR(255) NOT NULL,   -- само значение
     value_type VARCHAR(20) NOT NULL,   -- 'int', 'float', 'string', 'picture'
 
-    position INT,
+    position INT NOT NULL,
     FOREIGN KEY (enum_class_id) REFERENCES enum_classes(id) ON DELETE CASCADE
 );
 
