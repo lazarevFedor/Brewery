c5538724c9beb7eca2c8c512a7405e98eea39b0c
fedorlazarev <lazarevfedor3@gmail.com>
2026-04-20 18:20:01 +0300

    feat(db): add docstrings for methods

--- internal/repository/enum.go
+++ internal/repository/enum.go
@@ -12,26 +12,45 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// EnumRepository определяет контракт для взаимодействия с классами и значениями перечислений
 type EnumRepository interface {
+
+	// InsertEnumClass сохраняет новую сущность EnumClass в хранилище.
 	InsertEnumClass(ctx context.Context, enumClass entities.EnumClass) (uint, error)
+
+	// UpdateEnumClass обновляет сущность EnumClass в хранилище.
 	UpdateEnumClass(ctx context.Context, id uint, updates map[string]any) error
+
+	// DeleteEnumClassByID удаляет сущность EnumClass из хранилища.
 	DeleteEnumClassByID(ctx context.Context, id uint) error
+
+	// GetEnumClasses получает список сущностей EnumClass по заданным имени таблицы и поля.
 	GetEnumClasses(ctx context.Context, entity, field string) ([]entities.EnumClass, error)
 
+	// InsertEnumValue сохраняет новую сущность EnumValue в хранилище.
 	InsertEnumValue(ctx context.Context, enumValue entities.EnumValue) (uint, error)
+
+	// UpdateEnumValue обновляет сущность EnumValue в хранилище.
 	UpdateEnumValue(ctx context.Context, id uint, value any, position *int) error
+
+	// DeleteEnumValueByID удаляет сущность EnumValue из хранилища.
 	DeleteEnumValueByID(ctx context.Context, id uint) error
+
+	// GetEnumValues получает список сущностей EnumClass по заданным имени таблицы, поля и типу значения.
 	GetEnumValues(ctx context.Context, entity, field, valueType string) ([]entities.EnumValue, error)
 }
 
+// EnumPostgres хранит в себе пул подключений к БД
 type EnumPostgres struct {
 	Pool *pgxpool.Pool
 }
 
+// NewEnumPostgres создает новый репозиторий БД
 func NewEnumPostgres(pool *pgxpool.Pool) *EnumPostgres {
 	return &EnumPostgres{Pool: pool}
 }
 
+// InsertEnumClass сохраняет новую сущность EnumClass в хранилище.
 func (e *EnumPostgres) InsertEnumClass(ctx context.Context, enumClass entities.EnumClass) (uint, error) {
 	if e.Pool == nil {
 		return 0, errors.New("pool is nil")
@@ -54,6 +73,7 @@ func (e *EnumPostgres) InsertEnumClass(ctx context.Context, enumClass entities.E
 	return enumID, nil
 }
 
+// UpdateEnumClass обновляет сущность EnumClass в хранилище.
 func (e *EnumPostgres) UpdateEnumClass(ctx context.Context, id uint, updates map[string]any) error {
 	if e.Pool == nil {
 		return errors.New("pool is nil")
@@ -74,6 +94,7 @@ func (e *EnumPostgres) UpdateEnumClass(ctx context.Context, id uint, updates map
 	return nil
 }
 
+// DeleteEnumClassByID удаляет сущность EnumClass из хранилища.
 func (e *EnumPostgres) DeleteEnumClassByID(ctx context.Context, id uint) error {
 	if e.Pool == nil {
 		return errors.New("pool is nil")
@@ -94,6 +115,7 @@ func (e *EnumPostgres) DeleteEnumClassByID(ctx context.Context, id uint) error {
 	return nil
 }
 
+// GetEnumClasses получает список сущностей EnumClass по заданным имени таблицы и поля.
 func (e *EnumPostgres) GetEnumClasses(ctx context.Context, entity, field string) ([]entities.EnumClass, error) {
 	if e.Pool == nil {
 		return nil, errors.New("pool is nil")
@@ -147,18 +169,22 @@ func scanEnumClass(row pgx.Row) (*entities.EnumClass, error) {
 	return &class, nil
 }
 
+// InsertEnumValue сохраняет новую сущность EnumValue в хранилище.
 func (e *EnumPostgres) InsertEnumValue(ctx context.Context, enumValue entities.EnumValue) (uint, error) {
 	return 0, nil
 }
 
+// UpdateEnumValue обновляет сущность EnumValue в хранилище.
 func (e *EnumPostgres) UpdateEnumValue(ctx context.Context, id uint, value any, position *int) error {
 	return nil
 }
 
+// DeleteEnumValueByID удаляет сущность EnumValue из хранилища.
 func (e *EnumPostgres) DeleteEnumValueByID(ctx context.Context, id uint) error {
 	return nil
 }
 
+// GetEnumValues получает список сущностей EnumClass по заданным имени таблицы, поля и типу значения.
 func (e *EnumPostgres) GetEnumValues(ctx context.Context, entity, field, valueType string) ([]entities.EnumValue, error) {
 	return nil, nil
 }
--- internal/repository/queries/enums.go
+++ internal/repository/queries/enums.go
@@ -11,6 +11,7 @@ const (
 	enumValuesTable  = "enum_values"
 )
 
+// InsertEnumClass возвращает запрос на вставку класса перечисления.
 func InsertEnumClass(enumClass entities.EnumClass) sq.InsertBuilder {
 	data := map[string]any{
 		"enum_type":   enumClass.Type,
@@ -29,6 +30,7 @@ func InsertEnumClass(enumClass entities.EnumClass) sq.InsertBuilder {
 		Suffix("RETURNING id")
 }
 
+// UpdateEnumClass возращает запрос на обновление класса перечисления.
 func UpdateEnumClass(id uint, updates map[string]any) sq.UpdateBuilder {
 	return psql.
 		Update(enumClassesTable).
@@ -36,12 +38,14 @@ func UpdateEnumClass(id uint, updates map[string]any) sq.UpdateBuilder {
 		Where(sq.Eq{"id": id})
 }
 
+// DeleteEnumClass возвращает запрос на удаление класса перечисления.
 func DeleteEnumClass(id uint) sq.DeleteBuilder {
 	return psql.
 		Delete(enumClassesTable).
 		Where(sq.Eq{"id": id})
 }
 
+// SelectEnumClasses возвращает запрос на получения классов перечислений по имени таблицы и поля.
 func SelectEnumClasses(entity, field string) sq.SelectBuilder {
 	return psql.Select(
 		"id",
