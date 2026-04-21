c877fda7d0d977c216745a50d8552559de1f274d
yulia <balevajued@gmail.com>
2026-04-21 23:29:25 +0300

    fix:

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -24,9 +24,9 @@ type BeerService interface {
 	UpdateBeer(ctx context.Context, id uint, updates map[string]any) (uint, error)
 	DeleteBeer(ctx context.Context, id uint) error
 	GetAllBeers(ctx context.Context, limit, offset uint64) ([]entities.Beer, error)
-	GetBeerReviews(ctx context.Context) ([]entities.ProductCategory, error)
+	GetBeerReviews(ctx context.Context, limit, offset uint64, beerid uint) ([]entities.Review, error)
 	CreateReview(ctx context.Context, review *entities.Review) (uint, error)
-	UpdateReview(ctx context.Context, id uint, updates map[string]any) (uint, error)
+	UpdateReview(ctx context.Context, id uint, updates map[string]any) error
 	DeleteReview(ctx context.Context, id uint) error
 }
 
