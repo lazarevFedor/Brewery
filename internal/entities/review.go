9c119a2c02c7f95c5077467fcd788cf6d30b5689
Kirill Belyaev <kirill10121415@gmail.com>
2026-05-01 17:23:33 +0300

    fix(entity): fix review rating type

--- internal/entities/review.go
+++ internal/entities/review.go
@@ -2,10 +2,10 @@ package entities
 
 // Review представляет полное описание отзыва о пиве.
 type Review struct {
-	ID     uint    `json:"id" info:"ID записи в бд"`
-	Body   string  `json:"body" info:"Комментарий отзыва"`
-	BeerID uint    `json:"beer_id" info:"ID напитка, к которому прикреплен отзыв"`
-	Rating float32 `json:"rating" info:"Оценка напитка"`
+	ID     uint   `json:"id" info:"ID записи в бд"`
+	Body   string `json:"body" info:"Комментарий отзыва"`
+	BeerID uint   `json:"beer_id" info:"ID напитка, к которому прикреплен отзыв"`
+	Rating uint   `json:"rating" info:"Оценка напитка"`
 }
 
 // Reviews представляет собой массив отзывов о пиве.
