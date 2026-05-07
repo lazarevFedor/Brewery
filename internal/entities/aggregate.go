979dfefdd6d613d15e7ea9eac751366260ed8acd
fedorlazarev <lazarevfedor3@gmail.com>
2026-05-07 23:01:31 +0300

    feat(entities): enhance Aggregate and Parameter structures with JSON tags and descriptions

--- internal/entities/aggregate.go
+++ internal/entities/aggregate.go
@@ -1,12 +1,16 @@
 // Package entities содержит слой сущностей
 package entities
 
+// Aggregate представляет собой структуру, описывающую агрегат.
 type Aggregate struct {
-	ID                uint
-	Name              string
-	Description       string
-	NumericParameters []int
-	EnumParameters    []int
+	ID                uint   `json:"id,omitempty" info:"ID агрегата"`
+	Name              string `json:"name,omitempty" info:"Имя агрегата"`
+	Description       string `json:"description,omitempty" info:"Описание товара"`
+	NumericParameters []int  `json:"numeric_param_ids,omitempty" info:"Список численный параметров"`
+	EnumParameters    []int  `json:"enum_param_ids,omitempty" info:"Список параметров-перечислений"`
 }
 
+// Aggregates представляет собой срез агрегатов.
+//
+//easyjson:json
 type Aggregates []Aggregate
--- internal/entities/parameter.go
+++ internal/entities/parameter.go
@@ -1,19 +1,29 @@
 // Package entities содержит слой сущностей
 package entities
 
+// NumericParameter представляет собой структуру, описывающую численный параметр.
 type NumericParameter struct {
-	ID         uint
-	MinValue   int
-	MaxValue   int
-	FieldName  string
-	EntityName string
+	ID          uint   `json:"id,omitempty" info:"ID численного параметра"`
+	MinValue    int    `json:"min_val,omitempty" info:"Минимальное значение параметра"`
+	MaxValue    int    `json:"max_val,omitempty" info:"Максимальное значение параметра"`
+	FieldName   string `json:"field_name,omitempty" info:"Имя поля сущности"`
+	EntityName  string `json:"entity_name,omitempty" info:"Имя сущности, к которой относится параметр"`
+	Inheritable bool   `json:"inheritable,omitempty" info:"Флаг, указывающий, может ли параметр наследоваться от родительской категории"`
 }
 
+// NumericParameters представляет собой срез численных параметров.
+//
+//easyjson:json
 type NumericParameters []NumericParameter
 
+// EnumParameter представляет собой структуру, описывающую параметр-перечисление.
 type EnumParameter struct {
-	ID          uint
-	EnumClassID uint
+	ID          uint `json:"id,omitempty" info:"ID параметра-перечисления"`
+	EnumClassID uint `json:"enum_class_id,omitempty" info:"ID класса перечисления"`
+	Inheritable bool `json:"inheritable,omitempty" info:"Флаг, указывающий, может ли параметр наследоваться от родительской категории"`
 }
 
+// EnumParameters представляет собой срез параметров-перечислений.
+//
+//easyjson:json
 type EnumParameters []EnumParameter
