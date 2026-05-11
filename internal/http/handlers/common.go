f7f3701e7b002515ccd2eb8c6e3db68227ad0694
yulia <balevajued@gmail.com>
2026-05-11 22:32:06 +0300

    fix:

--- internal/http/handlers/common.go
+++ internal/http/handlers/common.go
@@ -9,8 +9,10 @@ const (
 )
 
 type Handlers struct {
-	CategoryHandler CategoriesHandlers
-	BeersHandler    BeersHandlers
-	ReviewHandler   ReviewsHandlers
-	EnumHandler     EnumHandlers
+	CategoryHandler      CategoriesHandlers
+	BeersHandler         BeersHandlers
+	ReviewHandler        ReviewsHandlers
+	EnumHandler          EnumHandlers
+	CategoryParamHandler ParametersHandlers
+	AggregateHandler     AggregateHandlers
 }
