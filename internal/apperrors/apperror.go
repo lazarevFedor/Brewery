c072bfeb560be212629a11d15f83681da5c19f50
Kirill Belyaev <kirill10121415@gmail.com>
2026-05-12 21:07:22 +0300

    fix: transfer helpers into main apperror.go

--- internal/apperrors/apperror.go
+++ internal/apperrors/apperror.go
@@ -1,6 +1,8 @@
 // Package apperrors хранит в себе структуру ошибки приложения
 package apperrors
 
+import "net/http"
+
 type APIError struct {
 	ErrorCode string `json:"error"`
 	Message   string `json:"message"`
@@ -12,7 +14,40 @@ type AppError struct {
 	Err        error
 }
 
-func (e *AppError) Error() string {
+func newAppError(errorCode, message string, status int, err error) *AppError {
+	apiErr := APIError{
+		ErrorCode: errorCode,
+		Message:   message,
+	}
+
+	return &AppError{
+		APIErr:     apiErr,
+		HTTPStatus: status,
+		Err:        err,
+	}
+}
+
+func NotFound(message string, err error) *AppError {
+	return newAppError(CodeNotFound, message, http.StatusNotFound, err)
+}
+
+func Validation(message string, err error) *AppError {
+	return newAppError(CodeValidation, message, http.StatusUnprocessableEntity, err)
+}
+
+func Conflict(message string, err error) *AppError {
+	return newAppError(CodeConflict, message, http.StatusConflict, err)
+}
+
+func Internal(err error) *AppError {
+	return newAppError(CodeInternalError, "Unexpected error occurred", http.StatusInternalServerError, err)
+}
+
+func BadRequest(message string, err error) *AppError {
+	return newAppError(CodeBadRequest, message, http.StatusBadRequest, err)
+}
+
+func (e AppError) Error() string {
 	if e.Err != nil {
 		return e.Err.Error()
 	}
